757fac7e2dd8ab121bfc6c447d5d9892779160f0
sephirothz87 <sephiroth.zhong@gmail.com>
2015-07-15 16:41:16 +0800

    local commit

---
+++ gen/com/example/testgit2015_07_15_160655/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.testgit2015_07_15_160655;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
